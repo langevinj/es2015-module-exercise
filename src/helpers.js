b683b5016a6f4fe2b6e59ceed0304b2c53558e60
Jeremy Langevin <langevinj832@gmail.com>
2020-11-13 09:13:27 -0500

    basic module complete

--- src/helpers.js
+++ src/helpers.js
@@ -4,12 +4,12 @@ function choice(items){
 }
 
 function remove(items, item){
-    let idx = items.findIndex(item);
+    let idx = items.indexOf(item);
     if(idx === -1){
         return undefined
     }
-    let removed = items.splice(idx,1)
-    return removed;
+    items.splice(idx,1)
+    return items;
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { arrayOfFruits } from "./foods";
 import { choice, remove } from "./helpers";
 
+const RANDOMFRUIT = choice(arrayOfFruits);
+console.log(`I'd like one ${RANDOMFRUIT}, please.`);
+console.log(`Here you go: ${RANDOMFRUIT}`);
+console.log(`Delicious! May I have another?`);
+let remainingFruit = remove(arrayOfFruits, RANDOMFRUIT);
+console.log(`I'm sorry, we're all out. We have ${remainingFruit.length} left.`);
